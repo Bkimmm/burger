9fafafaea372c55a3684c138c171d96d28a38d2b
Bryan Kim <bkim0826@gmail.com>
2022-05-16 19:02:16 -0400

    Mui

--- client/app.jsx
+++ client/app.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ResponsiveAppBar from './components/navbar.jsx'
+// import NavBar from './components/navbar.jsx'
+// import NavBar from './components/navbar.jsx'
+// import NavBar from './components/navbar.jsx'
+// import NavBar from './components/navbar.jsx'
+// import NavBar from './components/navbar.jsx'
+
 
 function App(){
     return(
         <BrowserRouter>
+        <ResponsiveAppBar/>
         <div>
             <h1>Hello World!</h1>
         </div>
-            
-
+        
         </BrowserRouter>
     );
 };
+
+
+
 
 export default App;
