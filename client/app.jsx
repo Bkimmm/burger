3011198bccda27047fdbf0adb4a9a4b83ef5c6c3
Bryan Kim <bkim0826@gmail.com>
2022-05-19 12:32:58 -0400

    demo box

--- client/app.jsx
+++ client/app.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import ResponsiveAppBar from './components/navbar.jsx';
 import slideShow from './components/slideshow.jsx';
-
+import Demo from './components/Demo.jsx';
 // import NavBar from './components/navbar.jsx'
 // import NavBar from './components/navbar.jsx'
 // import NavBar from './components/navbar.jsx'
 // import NavBar from './components/navbar.jsx'
 // import NavBar from './components/navbar.jsx'
+import MainContainer from './components/mainContainer.jsx'
 
 
 function App(){
     return(
         <BrowserRouter>
-        <ResponsiveAppBar/>
-        <slideShow />
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-        
+        {/* <ResponsiveAppBar/> */}
+        <MainContainer/>
         </BrowserRouter>
     );
 };
--- client/components/mainContainer.jsx
+++ client/components/mainContainer.jsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
+import Demo from './Demo.jsx';
 
 
 function mainContainer () {
-    
+    // const [query, setQuery] = useState('');
+    return(
+        <div>
+            <h2>Demo</h2>
+            {/* <Demo query = {query}/> */}
+            <input>
+            </input>
+            <button onClick={runQuery}>
+                Submit
+                </button>
+        </div>
+    )
 }
 
-
-
-
-
-
-
-
+export default mainContainer;
 
 
